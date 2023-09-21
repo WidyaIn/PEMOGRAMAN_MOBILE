174db1b8d350e13b23c4eeb2cd40dc6fe86eebd3
WidyaIn <91048154+WidyaIn@users.noreply.github.com>
2023-09-21 10:15:35 +0700

    Add files via upload

---
+++ week4/praktikum5.dart
@@ -0,0 +1,31 @@
+(int, int) tukar((int, int) record) {
+  var (a, b) = record;
+  return (b, a);
+}
+
+void main() {
+  // var record = ('first', a: 2, b: true, 'last');
+  // print(record);
+
+  var record = (1, 2);
+  print(record);
+  print(tukar(record));
+
+  // Record type annotation in a variable declaration:
+  (String, int) mahasiswa = ('Widya Indah Puspita Sari', 2141720034);
+  print(mahasiswa);
+
+  // var mahasiswa2 = ('first', a: 2, b: true, 'last');
+
+  // print(mahasiswa2.$1); // Prints 'first'
+  // print(mahasiswa2.a); // Prints 2
+  // print(mahasiswa2.b); // Prints true
+  // print(mahasiswa2.$2); // Prints 'last'
+
+  var mahasiswa2 = ('Widya Indah Puspita Sari', a: 2141720034, b: true, 'last');
+
+  print(mahasiswa2.$1);
+  print(mahasiswa2.a);
+  print(mahasiswa2.b);
+  print(mahasiswa2.$2);
+}
